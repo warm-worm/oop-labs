77d5692eedc071a2d76b23f079924f96deb483c5
warm-worm <ola.korniak@gmail.com>
2025-10-17 15:29:32 +0200

    Arrays

--- ArraysAndStrings/Program.cs
+++ ArraysAndStrings/Program.cs
@@ -14,9 +14,24 @@ string[] names = {
 // After last element should be ".".
 void PrintGroups(string[] t, int perLine)
 {
+    for (
+        int i = 0; i < t.Length; i++)
+    {
+        Console.Write(t[i]);
 
-    // Write required code.
-
+        if (i == t.Length - 1)
+        {
+            Console.WriteLine(".");
+        }
+        else if ((i + 1) % perLine == 0)
+        {
+            Console.WriteLine(",");
+        }
+        else
+        {
+            Console.Write(", ");
+        }
+    }
 }
 
 
@@ -27,9 +42,26 @@ void PrintGroups(string[] t, int perLine)
 
 void PrintColumns(string[] t, int perLine, int width)
 {
+    for (int i = 0; i < t.Length; i++)
+    {
+        string element = t[i];
 
-    // Write required code.
+        if (element.Length > width)
+        {
+            element = element.Substring(0, width);
+        }
 
+        Console.Write(element.PadRight(width));
+
+        if ((i + 1) % perLine == 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.Write("| ");
+        }
+    }
 }
 
 
