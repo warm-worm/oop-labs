abf43860f4beb6d41aacd316d84208890a7bb7c6
warm-worm <ola.korniak@gmail.com>
2025-11-04 12:19:01 +0100

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,12 @@
+﻿namespace Simulator;
+
+public class Animals
+{
+    public required string Description { get; init; }
+    public uint Size { get; set; } = 3;
+
+    public string Info
+    {
+        get { return $"{Description} <{Size}>"; }
+    }
+}
---
+++ Simulator/Creature.cs
@@ -0,0 +1,27 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature()
+    {
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I am {Name}!");
+    }
+
+    public string Info
+    {
+        get { return $"{Name} ({Level})"; }
+    }
+}
