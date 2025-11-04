a041c0e13457c5d962cd9f8dfd33ef382b598ca6
warm-worm <ola.korniak@gmail.com>
2025-11-04 13:14:41 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -2,11 +2,48 @@
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string _description = "Unnamed"; //wartosc domyslna zeby uniknac ostrzezenia kompilatora
+
+    public required string Description
+    {
+        get { return _description; }
+        init //bo ma dzialac tylko przy inicjalizacji
+        {
+            string processedDesc = value;
+
+            if (string.IsNullOrWhiteSpace(processedDesc))
+            {
+                processedDesc = "###";
+            }
+            else
+            {
+                processedDesc = processedDesc.Trim();
+            }
+
+            if (processedDesc.Length > 15) //max 15 znakow
+            {
+                processedDesc = processedDesc.Substring(0, 15);
+                processedDesc = processedDesc.TrimEnd();
+            }
+
+            while (processedDesc.Length < 3) //min 3 znaki
+            {
+                processedDesc += "#";
+            }
+
+            if (char.IsLower(processedDesc[0])) //pierwsza wielka litera
+            {
+                processedDesc = char.ToUpper(processedDesc[0]) + processedDesc.Substring(1);
+            }
+
+            _description = processedDesc;
+        }
+    }
+
     public uint Size { get; set; } = 3;
 
     public string Info
     {
-        get { return $"{Description} <{Size}>"; }
+        get { return $"{_description} <{Size}>"; }
     }
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,8 +2,73 @@
 
 public class Creature
 {
-    public string Name { get; set; }
-    public int Level { get; set; }
+    private string _name = "Unknown"; //wartosc domyslna name
+    private int _level = 1; //wartosc domyslna level
+
+    private bool _nameInitialized = false; //czy wartosc byla juz ustawiona
+    private bool _levelInitialized = false;
+
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            if (_nameInitialized) return;
+
+            string processedName = value;
+
+            if (string.IsNullOrWhiteSpace(processedName)) //usuwanie spacji
+            {
+                processedName = "###"; //brakujace znaki
+            }
+            else
+            {
+                processedName = processedName.Trim();
+            }
+
+            if (processedName.Length > 25) //najwyzej 25 znakow
+            {
+                processedName = processedName.Substring(0, 25);
+                processedName = processedName.TrimEnd(); //uwuwanie spacji na koncu
+            }
+
+            while (processedName.Length < 3) //3 znaki min
+            {
+                processedName += "#";
+            }
+
+            if (char.IsLower(processedName[0])) //pierwsza wielka litera
+            {
+                processedName = char.ToUpper(processedName[0]) + processedName.Substring(1);
+            }
+
+            _name = processedName;
+            _nameInitialized = true; //ustawienie flagi
+        }
+    }
+
+    public int Level
+    {
+        get { return _level; }
+        set
+        {
+            if (_levelInitialized) return; //nadajemy tylko raz
+
+            int processedLevel = value;
+
+            if (processedLevel < 1) //1-10
+            {
+                processedLevel = 1;
+            }
+            else if (processedLevel > 10)
+            {
+                processedLevel = 10;
+            }
+
+            _level = processedLevel;
+            _levelInitialized = true; //flaga
+        }
+    }
 
     public Creature(string name, int level = 1)
     {
@@ -13,11 +78,20 @@ public class Creature
 
     public Creature()
     {
+
     }
 
     public void SayHi()
     {
         Console.WriteLine($"Hi, I am {Name}!");
+    }
+
+    public void Upgrade() //awans o jeden poziom, max 10
+    {
+        if (_level < 10)
+        {
+            _level++;
+        }
     }
 
     public string Info
